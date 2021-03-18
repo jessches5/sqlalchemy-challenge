d33ae7076a6f7f813a6d18174c3807b82ab6ace5
Jessica Chester <chesterjessica10@gmail.com>
2021-03-17 23:50:53 -0500

    final

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import numpy as np
-
+import json
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import datetime as dt
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 
 
 #################################################
